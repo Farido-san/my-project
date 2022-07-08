c33e4f087eeba6dc3c87e73af29f2bc9e45988ed
Farid Jamil <89650023+Farido-san@users.noreply.github.com>
2022-07-09 01:03:52 +0800

    delete wikiwp directory
    tes

--- wikiwp/searchform.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo '<form role="search" method="get" class="search-form" action="'.home_url( '/' ).'">',
-		 '<label>',
-
-		 '<input type="search" class="search-field" placeholder="'.__( 'Search', 'wikiwp' ).'" value="'.get_search_query().'" name="s" title="'.__( 'Search', 'wikiwp' ).'" />',
-		 '</label>',
-		 '<input type="submit" class="search-submit" value="'.__( 'Search', 'wikiwp' ).'" />',
-		 '</form>';
